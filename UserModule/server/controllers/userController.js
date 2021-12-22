b2354e79cb3c3fd5eb2cd4ef46ae5e4ddcd92351
Nurullah Kucuk <81354718+nrllhkck@users.noreply.github.com>
2021-12-22 15:42:17 +0300

    crud added

--- UserModule/server/controllers/userController.js
+++ UserModule/server/controllers/userController.js
@@ -1,33 +1,89 @@
 const db = require("../models");
 const sequelize = db.sequelize;
-const User = db.User;
 
 const getListOfUsers = (req, res) => {
-  res.send("[get] getListOfUsers");
+  sequelize
+    .query("CALL getListOfUsers ()")
+    .then((v) => {
+      res.status(201).json({ status: "success", message: "Users found", users: v });
+    })
+    .catch((err) => {
+      res.status(500).json({ status: "error", message: err });
+    });
 };
 
 const createUser = (req, res) => {
   const { username, user_name, user_surname, user_password, user_email, user_type } = req.body;
   sequelize
-  .query('CALL createUser (:_username, :_user_name, :_user_surname, :_user_password, :_user_email, :_user_type)', 
-        {replacements: { _username: username, _user_name: user_name, _user_surname: user_surname, _user_password: user_password, _user_email: user_email, _user_type: user_type }})
-  .then((v)=>{
-    console.log(v);
-    res.status(201).json({status: "success", message: "User created", user: v[0]});
-  })
-  .catch(e=>console.log(e));
+    .query(
+      "CALL createUser (:_username, :_user_name, :_user_surname, :_user_password, :_user_email, :_user_type)",
+      {
+        replacements: {
+          _username: username,
+          _user_name: user_name,
+          _user_surname: user_surname,
+          _user_password: user_password,
+          _user_email: user_email,
+          _user_type: user_type,
+        },
+      },
+    )
+    .then((v) => {
+      res.status(201).json({ status: "success", message: "User created", user: v[0] });
+    })
+    .catch((err) => {
+      res.status(500).json({ status: "error", message: err });
+    });
 };
 
 const getUserInfo = (req, res) => {
-  res.send("[get] getUserInfo");
+  const user_id = req.params.id;
+  sequelize
+    .query("CALL getUserInfo (:_user_id)", { replacements: { _user_id: user_id } })
+    .then((v) => {
+      res.status(201).json({ status: "success", message: "User found", user: v });
+    })
+    .catch((err) => {
+      res.status(500).json({ status: "error", message: err });
+    });
 };
 
 const updateUser = (req, res) => {
-  res.send("[put] updateUser");
+  const user_id = req.params.id;
+  const { username, user_name, user_surname, user_password, user_email, user_type } = req.body;
+  sequelize
+    .query(
+      "CALL updateUser (:_user_id, :_username, :_user_name, :_user_surname, :_user_password, :_user_email, :_user_type)",
+      {
+        replacements: {
+          _user_id: user_id,
+          _username: username,
+          _user_name: user_name,
+          _user_surname: user_surname,
+          _user_password: user_password,
+          _user_email: user_email,
+          _user_type: user_type,
+        },
+      },
+    )
+    .then((v) => {
+      res.status(201).json({ status: "success", message: "User updated", user: v[0] });
+    })
+    .catch((err) => {
+      res.status(500).json({ status: "error", message: err });
+    });
 };
 
 const deleteUser = (req, res) => {
-  res.send("[delete] deleteUser");
+  const user_id = req.params.id;
+  sequelize
+    .query("CALL deleteUser (:_user_id)", { replacements: { _user_id: user_id } })
+    .then((v) => {
+      res.status(201).json({ status: "success", message: "User deleted" });
+    })
+    .catch((err) => {
+      res.status(500).json({ status: "error", message: err });
+    });
 };
 
 module.exports = {
